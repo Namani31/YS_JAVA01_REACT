114a3b419d86179d34e5483aa14cbb62490db4ee
Namani31 <kwg531@naver.com>
2024-07-18 12:27:23 +0900

    Feat: `useSate` 적용하여 카운팅 시작

--- src/step11/Counter.jsx
+++ src/step11/Counter.jsx
@@ -1,16 +1,18 @@
 // 파일: src/step11/Counter.jsx
+// 2. useState 사용하는 방법
+import {useState} from "react";
+
 export function Counter(props) {
-    let count = 0;
-    
+    // const[읽기변수, 입력함수] = useState(초기값)
+    const[count, setCount] = useState(0);
 
     return (
         <div>
             <p id='msg'>총 {count}번 클릭했습니다.</p>
             <button onClick={() => {
-                count++;
-                let msgEle = document.getElementById('msg');
-                msgEle.innerText = `총 ${count}번 클릭했습니다.`
-                console.log('count=>', count);
+                // count++; 읽기 전용이라 직접 변경 불가능
+                setCount(count+1);
+                console.log('count=>', count+1);
                 }}>클릭</button>
         </div>
     )
