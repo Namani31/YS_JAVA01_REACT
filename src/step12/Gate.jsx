469de8cce191dea5bb9bb781b53484b012b4a0e8
Namani31 <kwg531@naver.com>
2024-07-19 09:30:47 +0900

    Feat: `추가` 버튼 누르면 +1 되도록 추가

--- src/step12/Gate.jsx
+++ src/step12/Gate.jsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
 export function Gate() {
+    const [isFull, setIsFull] = useState(false);
+    const [isEmpty, setIsEmpty] = useState(false);
+    const [count, setCount] = useState(0);
+    const increaseCount = () => {
+        let _count = count + 1;
+        setCount(_count);
+    }
     return (
-        <>
-        456
-        </>
+        <div style={{padding:16}}>
+            <p>총 {count}명 수용했습니다.</p>
+            <button onClick={increaseCount} disabled={isFull}>입장</button>
+            <button disabled={isEmpty}>퇴장</button>
+            <p style={{color:'blue'}}>정원이 가득 찼습니다.</p>
+        </div>
     );
 }
