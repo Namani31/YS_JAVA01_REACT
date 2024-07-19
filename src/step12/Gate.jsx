1f94a3d8b22a492ebef49500f4a178b077c1b5de
Namani31 <kwg531@naver.com>
2024-07-19 10:12:21 +0900

    Style: 로직 설명용 주석 추가

--- src/step12/Gate.jsx
+++ src/step12/Gate.jsx
@@ -1,21 +1,27 @@
 import { useState } from "react";
 
+// 최대 정원
 const MAX_CAPACITY = 10;
 export function Gate() {
-    const [isFull, setIsFull] = useState(false);
+    const [isFull, setIsFull] = useState(false); //최대 정원
     const [isEmpty, setIsEmpty] = useState(true); // 초기값 0명으로 설정하기 위해 true 값 넣음
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0); // 현재 입장객
+
+    // 입장객 1명 증가
     const increaseCount = () => {
         let _count = count + 1;
-        setIsFull(_count >= MAX_CAPACITY);
-        setIsEmpty(_count <= 0);
-        setCount(_count);
+        setIsFull(_count >= MAX_CAPACITY); // 최대 정원의 상태를 셋팅 10명 이상 >= isFull:true
+        // setIsFull 명령이 없으면 최대 인원에서 퇴장을 눌렀을 때의 상태를 활성화 시킬 수 없음
+        setIsEmpty(_count <= 0); // 비어있는 상태를 셋팅 0명 => isEmpty:true
+        setCount(_count); // 
     }
+
+    // 입장객 1명 감소
     const decreaseCount = () => {
         let _count = count - 1;
-        setIsFull(_count >= MAX_CAPACITY);
-        setIsEmpty(_count <= 0);
-        setCount(_count);
+        setIsFull(_count >= MAX_CAPACITY); // 최대 정원의 상태를 셋팅 10명 이상 >= isFull:true
+        setIsEmpty(_count <= 0); // 비어있는 상태를 셋팅 0명 => isEmpty:true
+        setCount(_count); // 해당 명령이 없으면 비어 있는 상태를 활성화 시킬 수 없음
     }
     return (
         <div style={{padding:16}}>
