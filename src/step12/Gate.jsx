edd54e99039a684b00bf2f6a7e9ffdc30bd52305
Namani31 <kwg531@naver.com>
2024-07-19 09:44:32 +0900

    Feat: 입장객 카운팅

--- src/step12/Gate.jsx
+++ src/step12/Gate.jsx
@@ -1,19 +1,30 @@
 import { useState } from "react";
 
+const MAX_CAPACITY = 10;
 export function Gate() {
     const [isFull, setIsFull] = useState(false);
-    const [isEmpty, setIsEmpty] = useState(false);
+    const [isEmpty, setIsEmpty] = useState(true); // 초기값 0명으로 설정하기 위해 true 값 넣음
     const [count, setCount] = useState(0);
     const increaseCount = () => {
         let _count = count + 1;
+        setIsFull(_count >= MAX_CAPACITY);
+        setIsEmpty(_count <= 0);
+        setCount(_count);
+    }
+    const decreaseCount = () => {
+        let _count = count - 1;
+        setIsFull(_count >= MAX_CAPACITY);
+        setIsEmpty(_count <= 0);
         setCount(_count);
     }
     return (
         <div style={{padding:16}}>
             <p>총 {count}명 수용했습니다.</p>
             <button onClick={increaseCount} disabled={isFull}>입장</button>
-            <button disabled={isEmpty}>퇴장</button>
-            <p style={{color:'blue'}}>정원이 가득 찼습니다.</p>
+            <button onClick={decreaseCount} disabled={isEmpty}>퇴장</button>
+            
+            {isFull && <p style={{color:'red'}}>정원이 가득 찼습니다.</p> }
+            {isEmpty && <p style={{color:'red'}}>입장객이 없습니다.</p>}
         </div>
     );
 }
