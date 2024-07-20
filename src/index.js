8851491cb30ba7768d2ce0c1989d405991b662f2
Namani <kwg531@naver.com>
2024-07-20 23:46:53 +0900

    Feat: 렌더링 된 엘리먼트 업데이트하기

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 // import ConfirmButton from './practice/ConfirmButton'; 
 // import {App} from './step14/App';
 // import LandingPage from './step14/LandingPage';
-import Library from './practice/Library';
+// import Library from './practice/Library';
+import Clock from './practice/Clock';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <>
-    {console.log('render')}
-    <Library/>
-  </>
-);
+
+setInterval(() => {
+  root.render(
+    <>
+      {console.log('render')}
+      <Clock/>
+    </>
+  );
+}, 1000)
