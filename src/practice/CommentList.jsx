a491150a5e4db5a16df9fcad3779e58c2f2ed7c8
Namani <kwg531@naver.com>
2024-07-21 01:55:46 +0900

    Feat: `Comment` 컴포넌트 한 개 더 추가하기

--- src/practice/CommentList.jsx
+++ src/practice/CommentList.jsx
@@ -5,6 +5,7 @@ function CommentList(props) {
     return (
         <div>
             <Comment name={"나마니"} comment={"안녕하세요, 나마니입니다."}/>
+            <Comment name={"진"} comment={"리액트 화이팅!"}/>
         </div>
     );
 }
