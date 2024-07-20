e2c12c423693a9ad11bec2a9a267a8449db14378
Namani <kwg531@naver.com>
2024-07-21 01:43:05 +0900

    Feat: 실험용 컴포넌트 실행

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 //파일정보:src/index.js
 import ReactDOM from 'react-dom/client';
+import React from 'react';
 // import ConfirmButton from './practice/ConfirmButton'; 
 // import {App} from './step14/App';
 // import LandingPage from './step14/LandingPage';
 // import Library from './practice/Library';
-import Clock from './practice/Clock';
+// import Clock from './practice/Clock';
+import CommentList from './practice/CommentList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-setInterval(() => {
-  root.render(
-    <>
-      {console.log('render')}
-      <Clock/>
-    </>
-  );
-}, 1000)
+root.render(
+  <>
+    <React.StrictMode>
+      <CommentList />
+    </React.StrictMode>
+  </>
+);
+
