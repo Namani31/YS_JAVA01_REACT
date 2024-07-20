fa01e7b44eb5bdf9c3c66b23fae7d5876c581fad
Namani <kwg531@naver.com>
2024-07-21 02:00:04 +0900

    Feat: 배열 이용하여 값 전달하기

--- src/practice/CommentList.jsx
+++ src/practice/CommentList.jsx
@@ -1,11 +1,32 @@
 import React from "react";
 import Comment from "./Comment";
 
+const comments = [
+    {
+        name: "나마니",
+        comment: "안녕하세요, 나마니입니다.",
+    },
+    {
+        name: "진",
+        comment: "리액트 화이팅!",
+    },
+    {
+        name: "아서",
+        comment: "리액트 재밌네",
+    },
+    {
+        name: "더치",
+        comment: "Have some faith!",
+    },
+]
 function CommentList(props) {
     return (
         <div>
-            <Comment name={"나마니"} comment={"안녕하세요, 나마니입니다."}/>
-            <Comment name={"진"} comment={"리액트 화이팅!"}/>
+            {comments.map((comment) => {
+                return (
+                    <Comment name={comment.name} comment={comment.comment} />
+                );
+            })}
         </div>
     );
 }
