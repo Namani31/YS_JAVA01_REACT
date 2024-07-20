e60507d57ca45c5bb0425514d90f7503e46007f9
Namani <kwg531@naver.com>
2024-07-20 23:27:52 +0900

    Feat: JSX를 사용하는 실습

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@
 import ReactDOM from 'react-dom/client';
 // import ConfirmButton from './practice/ConfirmButton'; 
 // import {App} from './step14/App';
-import LandingPage from './step14/LandingPage';
+// import LandingPage from './step14/LandingPage';
+import Library from './practice/Library';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
     {console.log('render')}
-    <LandingPage/>
+    <Library/>
   </>
 );
