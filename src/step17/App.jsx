67dc0c40b491bfaa81d35ab8fb1832e7e24c68a3
Namani31 <kwg531@naver.com>
2024-07-22 14:59:44 +0900

    Feat: 컴포넌트 연결

--- src/step17/App.jsx
+++ src/step17/App.jsx
@@ -1,15 +1,8 @@
-import { RouterTest1 } from './RouterTest1';
-// import { FormTest2 } from './FormTest2';
-// import { FormTest3 } from './FormTest3';
-// import { FormTest4 } from './FormTest4;'
-// import { FormTest5 } from './FormTest5;'
-// import { Reservation } from './Reservation';
-
-
-export function App() {
+import { RouterTest2 } from './RouterTest2'; // 파일 경로에 따라 수정
+export function App(){
   return (
     <>
-      <RouterTest1 />
+      <RouterTest2/>
     </>
   );
 }
