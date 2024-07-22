dd7ab195b00faef0f47499561b36ccfb09745bc2
Namani31 <kwg531@naver.com>
2024-07-22 09:32:07 +0900

    Style: `ListTest3.jsx`에 설명 주석 추가

--- src/step15/ListTest3.jsx
+++ src/step15/ListTest3.jsx
@@ -8,6 +8,8 @@ export function ListTest3() {
             {/* 
                 맵 함수에서 만들어지는 요소는
                 반드시 유니크한 키값을 가지고 있어야 함
+                유니크 값이 없다면 전체 순환을 하여 값을 찾아내야 하기 때문에
+                성능에 문제가 생김
              */}
             {animals.map(
                 (animal, index) => {
