23c1baea9cd699ccf01cca0e9ae38e9830622e59
Namani31 <kwg531@naver.com>
2024-07-22 14:12:00 +0900

    실습 페이지 저장, path 문제 발생

--- src/step16/App.jsx
+++ src/step16/App.jsx
@@ -1,11 +1,15 @@
 // import { FormTest1 } from './FormTest1';
 // import { FormTest2 } from './FormTest2';
 // import { FormTest3 } from './FormTest3';
-import { Reservation } from './Reservation';
+// import { FormTest4 } from './FormTest4;'
+import { FormTest5 } from './FormTest5;'
+// import { Reservation } from './Reservation';
+
+
 export function App() {
   return (
     <>
-      <Reservation />
+      <FormTest5 />
     </>
   );
 }
