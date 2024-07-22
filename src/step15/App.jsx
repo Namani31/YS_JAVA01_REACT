2d46039c95eaea8de30bd1d6a21c6b4004c1cb0e
Namani31 <kwg531@naver.com>
2024-07-22 10:02:32 +0900

    Feat: 배열 사용해서 값 출력하기

--- src/step15/App.jsx
+++ src/step15/App.jsx
@@ -1,11 +1,12 @@
 // import {ListTest1} from './ListTest1'; 
 // import {ListTest2} from './ListTest2'; 
-import {ListTest3} from './ListTest3';
+// import {ListTest3} from './ListTest3';
+import {ListTest4} from './ListTest4';
 
 export function App(){
   return (
     <>
-    <ListTest3/>
+    <ListTest4/>
     </>
   );
 }
