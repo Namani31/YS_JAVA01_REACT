3afccb60c7f7b8da612779e82699ba410cf633dc
Namani31 <kwg531@naver.com>
2024-07-22 11:46:33 +0900

    Feat: 여러 개의 입력 다루기 예제

--- src/step16/App.jsx
+++ src/step16/App.jsx
@@ -1,10 +1,11 @@
 // import { FormTest1 } from './FormTest1';
 // import { FormTest2 } from './FormTest2';
-import { FormTest3 } from './FormTest3';
+// import { FormTest3 } from './FormTest3';
+import { Reservation } from './Reservation';
 export function App() {
   return (
     <>
-      <FormTest3 />
+      <Reservation />
     </>
   );
 }
