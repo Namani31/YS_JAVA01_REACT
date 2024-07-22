39443a07a56255735d6ba4389634c6b00f6b4e08
Namani31 <kwg531@naver.com>
2024-07-22 16:30:07 +0900

    Feat: 검색 기능 구현

--- src/step17/pages/About2.jsx
+++ src/step17/pages/About2.jsx
@@ -1,12 +1,13 @@
-import {Link} from 'react-router-dom';
+import { Link ,Outlet} from "react-router-dom";
 
 export function About2() {
-    return (
-        <>
-            <h2>도움말2</h2>
-            <nav>
-                <Link to="details">도움말 상세보기</Link>
-            </nav>
-        </>
-    );
+   return (
+      <>
+         <h2>도움말2</h2>
+         <nav>
+            <Link to="details">도움말 상세보기</Link>
+         </nav>
+         <Outlet/>
+      </>
+   );
 }
