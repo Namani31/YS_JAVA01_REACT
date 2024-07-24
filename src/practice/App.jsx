a65d99883e245b89c28caa639a751dba41cdbb96
Namani31 <kwg531@naver.com>
2024-07-24 16:22:20 +0900

    Feat: 라우터 예시용 `App,jsx` 생성

--- src/practice/App.jsx
+++ src/practice/App.jsx
@@ -1,9 +1,32 @@
-import ConfirmButton from './ConfirmButton';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Home } from './Home';
+import { About } from './About';
+import { Contact } from './Contact';
 
-export function App(){
+// import ConfirmButton from './ConfirmButton';
+
+export function App() {
   return (
-    <div>
-      <ConfirmButton/>
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/content">Content</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Router path="/" element={<Home />} />
+        <Router path="/about" element={<About />} />
+        <Router path="/Contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
